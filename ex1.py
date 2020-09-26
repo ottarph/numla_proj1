8586a9fcc11ab5072a31e8485e384a8d98b058f9
Ottar Hellan <37851628+ottarph@users.noreply.github.com>
2020-09-26 20:31:19 +0200

    Work finished on exercise 1

--- ex1.py
+++ ex1.py
@@ -44,7 +44,6 @@ def main():
 
     h = 1 / (n + 1)
 
-    #L = build_L_sparse(n)
 
     b = np.ones(n**2) * h**2
     x_0 = np.ones(n**2)
@@ -73,10 +72,10 @@ def main():
     T_sor = end - start
     print('SOR ', i_sor, f'{T_sor*1e3:.2f} ms', np.linalg.norm(L @ x_sor - b), w)
 
-    iterations = np.array([i_jac, i_gs, i_sor])
-    times = np.array([T_jac, T_gs, T_sor])
+    iterationsA = np.array([i_jac, i_gs, i_sor])
+    timesA = np.array([T_jac, T_gs, T_sor])
 
-    print(times / iterations * 1000, 'ms')
+    print(f'Time per iteration, tolerance {TOL}', timesA / iterationsA * 1000, 'ms')
     
 
     plt.figure()
@@ -89,18 +88,54 @@ def main():
     plt.legend()
     plt.xlabel('Iterations')
     plt.ylabel('Relative residuals')
-    plt.xlim(0, np.amax(iterations))
+    plt.xlim(0, np.amax(iterationsA))
+
+
+    TOL = 1e-14
+    RTOL = TOL
     
+    #'''
+    start = time()
+    x_jac, i_jac, r_jac = jacobi_fp_sparse(L, b, x_0, tol=TOL, rtol=RTOL)
+    end = time()
+    T_jac = end - start
+    print('Jac ', i_jac, f'{T_jac*1e3:.2f} ms', np.linalg.norm(L @ x_jac - b))
+
+    #'''
+
+    #'''
+    start = time()
+    x_gs, i_gs, r_gs = f_gauss_seidel_sparse_fp(L, b, x_0, tol=TOL, rtol=RTOL)
+    end = time()
+    T_gs = end - start
+    print('GS ', i_gs, f'{T_gs*1e3:.2f} ms', np.linalg.norm(L @ x_gs - b))
+
+    #'''
+    w = 1.5
+    start = time()
+    x_sor, i_sor, r_sor = successive_over_relaxation_sparse(L, b, x_0, w, tol=TOL, rtol=RTOL)
+    end = time()
+    T_sor = end - start
+    print('SOR ', i_sor, f'{T_sor*1e3:.2f} ms', np.linalg.norm(L @ x_sor - b), w)
+
+    iterationsB = np.array([i_jac, i_gs, i_sor])
+    timesB = np.array([T_jac, T_gs, T_sor])
+
+    print(f'Time per iteration, tolerance {TOL}', timesB / iterationsB * 1000, 'ms')
+
     plt.figure()
 
-    pos = [0.25, 0.5, 0.75]
+    pos = np.array([0.25, 0.5, 0.75])
 
-    plt.bar(pos, times * 1000, width=0.1, color='black', alpha=0.6, linewidth=1, edgecolor='black')
+    plt.bar(pos, timesA * 1000, width=0.05, color='black', alpha=0.4, linewidth=1, edgecolor='black', label=r'TOL $= 10^{-7}$')
+    plt.bar(pos + 0.05, timesB * 1000, width=0.05, color='black', alpha=0.8, linewidth=1, edgecolor='black', label=r'TOL $= 10^{-14}$')
     plt.xticks(pos, ['Jacobi', 'Gauss-Seidel', rf'SOR, $\omega = {w}$'])
     plt.ylabel('Running time / ms')
+    plt.legend()
 
-    plt.figure()
 
+    TOL = 1e-7
+    RTOL = TOL
     ww = np.linspace(1.0, 1.5, 11)
     ww = np.append(ww, np.linspace(1.5 + (1.6-1.5)/10, 1.6, 10))
     ww = np.append(ww, np.linspace(1.6 + (2.0-1.6)/8, 2.0, 8))
@@ -109,6 +144,8 @@ def main():
     for i, w in enumerate(ww):
         _, it, _ = successive_over_relaxation_sparse(L, b, x_0, w, tol=TOL, rtol=RTOL)
         iters[i] = it
+    
+    plt.figure()
 
     plt.plot(ww, iters, 'k.-', linewidth=0.7)
     plt.axhline(np.amin(iters), linewidth=0.5, linestyle='dashed', color='black')
@@ -118,9 +155,14 @@ def main():
     plt.xlabel('$\omega$')
     plt.ylabel('Iterations')
 
+    TOL = 1e-7
+    RTOL = TOL
 
-
-    i_jac  = np.arange(1, r_jac.shape[0] + 1, dtype=float)
+    #i_jac  = np.arange(1, r_jac.shape[0] + 1, dtype=float)
+    #i_gs = np.arange(1, r_gs.shape[0] + 1, dtype=float)
+    _, _, r_jac = jacobi_fp_sparse(L, b, x_0, tol=TOL, rtol=RTOL)
+    i_jac = np.arange(1, r_jac.shape[0] + 1, dtype=float)
+    _, _, r_gs = f_gauss_seidel_sparse_fp(L, b, x_0, tol=TOL, rtol=RTOL)
     i_gs = np.arange(1, r_gs.shape[0] + 1, dtype=float)
     w = 1.57
     _, _, r_sor = successive_over_relaxation_sparse(L, b, x_0, w, tol=TOL, rtol=RTOL)
